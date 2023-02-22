02a81267d838c13f2246190b5574f66d9efb0a1b
Agusioma <terryterrence200@gmail.com>
2023-02-22 22:18:42 +0300

    LCD display

--- dht.ino
+++ dht.ino
@@ -1,34 +1,44 @@
+#include <LiquidCrystal.h> // includes the LiquidCrystal Library
 #include <dht.h>
-#define dataPin 8 // Defines pin number to which the sensor is connected
-dht DHT; // Creats a DHT object
+#define dataPin 8
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7)
+dht DHT;
+bool showcelciusorfarenheit = false;
 
 void setup() 
 {
-  Serial.begin(9600);
+  lcd.begin(16,2); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display
 }
+
 void loop() 
 {
-  //Uncomment whatever type you're using!
-  int readData = DHT.read22(dataPin);// DHT22/AM2302
- //int readData = DHT.read11(dataPin); // DHT11
-
-  float t = DHT.temperature; // Gets the values of the temperature
-  float h = DHT.humidity; // Gets the values of the humidity
-
-  // Printing the results on the serial monitor
-  Serial.print("Temperature = ");
-  Serial.print(t);
-  Serial.print(" ");
-  Serial.print((char)176);//shows degrees character
-  Serial.print("C | ");
-  Serial.print((t * 9.0) / 5.0 + 32.0);//print the temperature in Fahrenheit
-  Serial.print(" ");
-  Serial.print((char)176);//shows degrees character
-  Serial.println("F ");
-  Serial.print("Humidity = ");
-  Serial.print(h);
-  Serial.println(" % ");
-  Serial.println("");
-
-  delay(2000); // Delays 2 secods
+  int readData = DHT.read22(dataPin);
+  float t = DHT.temperature;
+  float h = DHT.humidity;
+  lcd.setCursor(0,0); // Sets the location at which subsequent text written to the LCD will be displayed
+  lcd.print("Temp.: "); // Prints string "Temp." on the LCD
+  //Print temperature value in Celcius and Fahrenheit every alternate cycle
+  if(showcelciusorfarenheit)
+  {
+    lcd.print(t); // Prints the temperature value from the sensor
+    lcd.print(" ");
+    lcd.print((char)223);//shows degrees character
+    lcd.print("C");
+    showcelciusorfarenheit = false;
+  }
+  else
+  {
+    lcd.print((t * 9.0) / 5.0 + 32.0); // print the temperature in Fahrenheit
+    lcd.print(" ");
+    lcd.print((char)223);//shows degrees character
+    lcd.print("F");
+    showcelciusorfarenheit = true;
+  }
+  
+  lcd.setCursor(0,1);
+  lcd.print("Humi.: ");
+  lcd.print(h);
+  lcd.print(" %");
+  delay(5000);
 }
